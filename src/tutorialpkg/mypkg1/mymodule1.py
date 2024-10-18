298cfc22adc1e0b3eddf048b4f0de953b2ee943c
zcecguo <zcecguo@ucl.ac.uk>
2024-10-18 14:20:16 +0100

    activity 2

--- src/tutorialpkg/mypkg1/mymodule1.py
+++ src/tutorialpkg/mypkg1/mymodule1.py
@@ -2,7 +2,8 @@ mock_database = {
     1: {'name': 'Alice', 'email': 'alice@example.com', 'age': 30},
     42: {'name': 'Bob', 'email': 'bob@example.com', 'age': 45},
 }
-
+from tutorialpkg.mypkg2.mymodule2_1 import calculate_area_of_circle
+from tutorialpkg.mypkg2.mymodule2_2 import fetch_user_data
 if __name__ == '__main__':
     # The functions are in the modules in mypkg2. You will need to import them.
 
@@ -14,3 +15,16 @@ if __name__ == '__main__':
     print(fetch_user_data(42, mock_database))
 
     # Locate the data file `paralmpics_raw.csv` relative to this file using pathlib.Path. Prove it exists.
+from pathlib import Path
+
+from pathlib import Path
+
+# This script is located in a subfolder so you need to navigate up to the parent (src) and then its parent (project root), then down to the 'data' directory and finally the .csv file
+csv_file = Path().parent.joinpath('data', 'paralympics_raw.csv')
+
+
+# Check if the file exists
+if csv_file.exists():
+    print(f"CSV file found: {csv_file}")
+else:
+    print("CSV file not found.")
